da216179431d83fa11c5f77a34e65614b9030325
jpromanonet <programming@jpromano.net>
2019-08-03 14:17:50 -0300

    Mile added

--- metricConverter.js
+++ metricConverter.js
@@ -3,7 +3,7 @@ document.getElementById("metricInput").addEventListener("input", function(e) {
   document.getElementById("output").style.visibility = "visible";
   let mts = e.target.value;
   document.getElementById("feetOutput").innerHTML = mts * 3.281 + " ft";
-  document.getElementById("kgOutput").innerHTML = lbs / 2.2046 + " Kg";
+  document.getElementById("mlOutput").innerHTML = mts / 1609.344 + " ml";
   document.getElementById("ozOutput").innerHTML = lbs * 16 + " Oz";
   document.getElementById("stOutput").innerHTML = lbs / 14 + " st";
   document.getElementById("tnOutput").innerHTML = lbs /  2204.623 + " tn";
