e79c3ac03f5eb16bf8bb6be230274896d2558d92
jpromanonet <programming@jpromano.net>
2019-08-03 14:37:15 -0300

    Milimeters and micrometers added

--- imperialConverter.js
+++ imperialConverter.js
@@ -5,6 +5,6 @@ document.getElementById("feetInput").addEventListener("input", function(e) {
   document.getElementById("mtsOutput").innerHTML = ft / 3.281 + " m";
   document.getElementById("kmOutput").innerHTML = ft / 3280.84 + " Km";
   document.getElementById("cmOutput").innerHTML = ft * 30.48 + " cm";
-  document.getElementById("stOutput").innerHTML = kg / 6.35 + " st";
-  document.getElementById("tnOutput").innerHTML = kg /  1000 + " tn";
+  document.getElementById("mmOutput").innerHTML = ft * 304.8 + " mm";
+  document.getElementById("mcmOutput").innerHTML = ft * 304800 + " mcn";
 });
