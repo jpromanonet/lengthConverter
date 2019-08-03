7902386a10de5b9e67a080d3cbcf117ae30bc6fa
jpromanonet <programming@jpromano.net>
2019-08-03 14:15:16 -0300

    Feet added

--- metricConverter.js
+++ metricConverter.js
@@ -1,8 +1,8 @@
 document.getElementById("output").style.visibility = "hidden";
-document.getElementById("lbsInput").addEventListener("input", function(e) {
+document.getElementById("metricInput").addEventListener("input", function(e) {
   document.getElementById("output").style.visibility = "visible";
-  let lbs = e.target.value;
-  document.getElementById("gramsOutput").innerHTML = lbs / 0.0022046 + " gr";
+  let mts = e.target.value;
+  document.getElementById("feetOutput").innerHTML = mts * 3.281 + " ft";
   document.getElementById("kgOutput").innerHTML = lbs / 2.2046 + " Kg";
   document.getElementById("ozOutput").innerHTML = lbs * 16 + " Oz";
   document.getElementById("stOutput").innerHTML = lbs / 14 + " st";
