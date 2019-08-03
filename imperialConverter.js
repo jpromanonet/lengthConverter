1eacb2b7f2b4e31cb62897ec0b9ae415b48cc38c
jpromanonet <programming@jpromano.net>
2019-08-03 14:33:17 -0300

    Meters, Km and cm added

--- imperialConverter.js
+++ imperialConverter.js
@@ -1,10 +1,10 @@
 document.getElementById("output").style.visibility = "hidden";
-document.getElementById("kgInput").addEventListener("input", function(e) {
+document.getElementById("feetInput").addEventListener("input", function(e) {
   document.getElementById("output").style.visibility = "visible";
-  let kg = e.target.value;
-  document.getElementById("gramsOutput").innerHTML = kg * 1000 + " gr";
-  document.getElementById("lbsOutput").innerHTML = kg * 2.2046 + " lbs";
-  document.getElementById("ozOutput").innerHTML = kg * 35.274 + " Oz";
+  let ft = e.target.value;
+  document.getElementById("mtsOutput").innerHTML = ft / 3.281 + " m";
+  document.getElementById("kmOutput").innerHTML = ft / 3280.84 + " Km";
+  document.getElementById("cmOutput").innerHTML = ft * 30.48 + " cm";
   document.getElementById("stOutput").innerHTML = kg / 6.35 + " st";
   document.getElementById("tnOutput").innerHTML = kg /  1000 + " tn";
 });
